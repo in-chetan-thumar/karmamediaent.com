70405f4a985acf00471b2148491aff3940c3a383
in-chetan-thumar <chetan.thumar@tiez.nl>
2023-11-03 16:51:51 +0530

    Update about.blade.php
    Remove section from about us page

--- www/resources/views/frontend/about.blade.php
+++ www/resources/views/frontend/about.blade.php
@@ -14,30 +14,16 @@
         <div class="contentdiv">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-6 col-md-12">
+                    <div class="col-lg-12 col-md-12">
                         <div class="aboutinfo">
                             <div class="heading mb-4">
                                 <h2>About Us</h2>
                             </div>
-                            <p>Karma Media And Entertainment is one of India's leading production houses,
-                                established in the
-                                year 2007 by Mr. Shailesh Singh who is known as Shaailesh R Singh in the business
-                                world.</p>
+                            <p>Karma Media And Entertainment is one of India's leading production houses, established in the year 2017 by Mr. Aditya Arun Singh.</p>
 
-                            <p>Shaailesh R Singh’s filmography as a producer, includes commercially successful
-                                movies as
-                                well as films that are critically acclaimed both nationally and internationally. In
-                                a career
-                                spanning 15 years, Shailesh has received numerous accolades for backing
-                                path-breaking
-                                cinema, which includes movies like Shahid, Madaari, Aligarh, Thalaivii and more.
-                            </p>
+                            <p>Our filmography as a production house, includes commercially successful movies as well as films that are critically acclaimed both nationally and internationally. In a career spanning 5 years, We have received numerous accolades for backing path-breaking cinema, which includes movies like Jabariya Jodi, Judgemental Hai kya, Thalaivii and more.</p>
 
-                            <p>Having spent more than a decade in producing movies and web-series across scales and
-                                genres,
-                                Karma Media And Entertainment has now etched its mark in the industry as a creator
-                                of
-                                different, real, and seamless cinematic experiences.</p>
+                            <p>Having spent more than Five years in producing movies and web-series across scales and genres, Karma Media And Entertainment has now etched its mark in the industry as a creator of different, real, and seamless cinematic experiences.</p>
 
                             <p>After setting and breaking the benchmarks in the Indian film industry with movies
                                 like Tanu
@@ -46,21 +32,6 @@
                                 overseas and venturing into international space with movies and digital projects as
                                 Karma
                                 Media And Entertainment INC in Atlanta, USA.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-12">
-                        <div class="aboutboxright">
-                            <div class="aboutsailesh">
-                                <div class="heading">
-                                    <h2>Shaailesh R. Singh</h2>
-                                </div>
-                                <p>A cinephile at heart, he is a filmmaker and a trendsetter producer who changed
-                                    the
-                                    perspective of cinema-goers by backing some path-breaking movies in the Indian
-                                    entertainment industry.</p>
-                            </div>
-                            <img src="{{asset('assets/frontend/images/shailesh.png')}}"
-                                alt=" " width="100%" />
                         </div>
                     </div>
                 </div>
